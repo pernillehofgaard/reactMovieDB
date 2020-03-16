89c77cae3178de4c476ee1fa47515c02dbb37626
Pernille Hofgaard <pernille.hofgaard@hotmail.com>
2020-03-16 16:47:50 +0100

    deploy

---
+++ src/components/elements/NotFound/NotFound.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NotFound = ()=>{
+  return(
+    <div>
+      <h1>Whops. This page doesn't exists</h1>
+    </div>
+  );
+}
+
+export default NotFound;
---
+++ src/config.js
@@ -0,0 +1,15 @@
+const API_URL = 'http://api.themoviedb.org/3/';
+const API_KEY = 'a3fab7a0c93c6a60a01cd2bc59e714f3';
+
+const IMAGE_BASE_URL = 'http://image.tmdb.org/t/p/';
+
+const BACKDROP_SIZE = 'w1280';
+const POSTER_SIZE = 'w500';
+
+export{
+  API_KEY,
+  API_URL,
+  IMAGE_BASE_URL,
+  BACKDROP_SIZE,
+  POSTER_SIZE 
+}
