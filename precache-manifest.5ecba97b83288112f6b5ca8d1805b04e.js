6ff8af1034d9c58ce67ff2e7ba779329775968a4
Pernille Hofgaard <pernille.hofgaard@hotmail.com>
2020-03-16 16:50:05 +0100

    Updates

---
+++ precache-manifest.5ecba97b83288112f6b5ca8d1805b04e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0eaa105cb36c00cda0def25445ee0994",
+    "url": "/reactMovieDB/index.html"
+  },
+  {
+    "revision": "6844b0174310a078663b",
+    "url": "/reactMovieDB/static/css/main.9753b19f.chunk.css"
+  },
+  {
+    "revision": "1929258b7629dbd19d15",
+    "url": "/reactMovieDB/static/js/2.ba5e7284.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/reactMovieDB/static/js/2.ba5e7284.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6844b0174310a078663b",
+    "url": "/reactMovieDB/static/js/main.5a2f940c.chunk.js"
+  },
+  {
+    "revision": "0a91cd67f82bbeb11372",
+    "url": "/reactMovieDB/static/js/runtime-main.b71cd1b1.js"
+  }
+]);
